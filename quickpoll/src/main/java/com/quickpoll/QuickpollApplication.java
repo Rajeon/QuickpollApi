370ddc0846a00fce2535c48dc03d85afa57b2e37
Ray Felton <frajeon@gmail.com>
2019-11-01 08:41:57 -0400

    cool

---
+++ quickpoll/src/main/java/com/quickpoll/QuickpollApplication.java
@@ -0,0 +1,11 @@
+package com.quickpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickpollApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(QuickpollApplication.class, args);
+	}
+}
---
+++ quickpoll/src/main/java/com/quickpoll/repository/OptionRepository.java
@@ -0,0 +1,8 @@
+package com.quickpoll.repository;
+
+import com.quickpoll.domain.Option;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OptionRepository extends CrudRepository<Option, Long> {
+
+}
